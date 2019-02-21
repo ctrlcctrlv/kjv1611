05327dc73e7825c177d2f4e1384149953e6ae494
Fredrick Brennan <copypaste@kittens.ph>
2019-02-21 18:38:06 +0800

    Put each kern subtable in own lookup
    No more dropping of GPOS table needed.
    
    @khaledhosny is a genius !
    
    Close fontforge/fontforge#3435

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# We use TTX to clean up the font after building it…
-# Why `-x GPOS`? See https://github.com/fontforge/fontforge/issues/3415
 fontforge -lang=py -script script.txt
 rm *.ttx
-ttx -x GPOS KJV1611.otf
+# We use TTX to clean up the font after building it…
+ttx KJV1611.otf
 rm KJV1611.otf
 ttx KJV1611.ttx
 # …and also generate a WOFF, if posssible
